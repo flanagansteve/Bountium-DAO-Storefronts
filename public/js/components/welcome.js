cbf484a6b7387bcb5f9d1d5a5d0862e6ab746ccd
Steve Flanagan <flanagan.s@husky.neu.edu>
2019-06-27 15:30:54 -0400

    yoof

--- public/js/components/welcome.js
+++ public/js/components/welcome.js
@@ -1,18 +1,14 @@
 var Welcome = React.createClass({
 
   render : function() {
-    var br = React.createElement("br", {});
     return React.createElement("div", {className:"container"},
       React.createElement("h1", {}, "Welcome!"),
-      React.createElement("div", {className:"view-store-form"},
-        React.createElement("h4", {}, "Shop at a Bountium-powered business"),
-        React.createElement("p", {}, "Example on the Ropsten network at: 0xa24af57cf89ac03a5b760e2955ed288266270cd2"),
-        React.createElement("input", {type:"text", id:"store-addr-input", placeholder:"Desired business's address"}),
-        br, br,
-        React.createElement("button",
-          {id:"store-addr-input-btn", onClick:() => {this.props.renderStore(document.getElementById("store-addr-input").value)}},
-        "Go!")
-      )
+      React.createElement("h4", {}, "Shop at a Bountium-powered business"),
+      React.createElement("p", {}, "Example on the Ropsten network at: 0xa24af57cf89ac03a5b760e2955ed288266270cd2"),
+      React.createElement("input", {type:"text", className:"form-control", id:"store-addr-input", placeholder:"Desired business's address"}),
+      React.createElement("button",
+        {id:"store-addr-input-btn", className: "btn btn-primary mt-2", onClick:() => {this.props.renderStore(document.getElementById("store-addr-input").value)}},
+      "Go!")
     )
   }
 });
