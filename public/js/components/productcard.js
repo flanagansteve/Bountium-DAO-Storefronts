aa9eaf11e71e65428b0f351d1b83f530a87e3f6c
Steve Flanagan <flanagan.s@husky.neu.edu>
2019-07-05 15:11:07 -0400

    fixed card body tag

--- public/js/components/productcard.js
+++ public/js/components/productcard.js
@@ -21,15 +21,13 @@ var ProductCard = React.createClass({
   render : function() {
     return React.createElement("a", {href:"#", className:"link-unstyled"},
       React.createElement("div", {className:"card d-flex", onClick:this.renderProduct},
+        React.createElement("img", {className:"card-img-top mx-auto d-block", alt:this.props.product.name, src:this.props.product.imageUrl}),
         React.createElement("div", {className:"card-body"},
-          React.createElement("img", {className:"card-img-top mx-auto d-block", alt:this.props.product.name, src:this.props.product.imageUrl}),
-          React.createElement("div", {className:"container"},
-            // TODO can we make the product name appear as a link w/o really being a link?
-            React.createElement("h5", {}, React.createElement("p", {className:"card-link"}, this.props.product.name)),
-            React.createElement("p", {}, this.props.product.description),
-            React.createElement("p", {}, "Price: " + web3.fromWei(this.props.product.price, "ether") + " ETH"),
-            React.createElement("button", {className:"btn btn-info mb-1", onClick:this.renderProduct}, "Order")
-          )
+          // TODO can we make the product name appear as a link w/o really being a link?
+          React.createElement("h5", {}, React.createElement("p", {className:"card-link"}, this.props.product.name)),
+          React.createElement("p", {}, this.props.product.description),
+          React.createElement("p", {}, "Price: " + web3.fromWei(this.props.product.price, "ether") + " ETH"),
+          React.createElement("button", {className:"btn btn-info mb-1", onClick:this.renderProduct}, "Order")
         )
       )
     );
